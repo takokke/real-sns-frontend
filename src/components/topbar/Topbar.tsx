cbff31dc3114b7406d8ef9f40dc3e57b1d615d99
takumi_Mac <>
2024-07-04 00:53:22 +0900

    topbar作成

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,7 +1,36 @@
+import { Chat, Search, Notifications } from '@mui/icons-material'
 import React from 'react'
+import "./Topbar.css"
 
 export default function Topbar() {
   return (
-    <div>Topbar</div>
+    <div className='topbarContainer'>
+      <div className='topbarLeft'>
+        <span className='logo'>Real SNS</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className='searchIcon'/>
+          <input 
+            type="text" 
+            className='searchInput' 
+            placeholder='探し物はなんですか？'
+          />          
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarItemIcons">
+          <div className="topbarIconItem">
+            <Chat/>
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications/>
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <img src="/assets/person/1.jpeg" alt="" className='topbarImg'/>          
+        </div>
+      </div>
+    </div>
   )
 }
